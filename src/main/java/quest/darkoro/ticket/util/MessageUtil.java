373879e36669c1c898c598113f44931bdf7aa573
Jean-Philippe K. <jean.kraemer@csbme.de>
2025-01-08 01:07:18 +0100

    0.6.2
    wait delete then send

--- src/main/java/quest/darkoro/ticket/util/MessageUtil.java
+++ src/main/java/quest/darkoro/ticket/util/MessageUtil.java
@@ -3,6 +3,7 @@ package quest.darkoro.ticket.util;
 import static net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle.PRIMARY;
 
 import java.awt.Color;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -22,28 +23,33 @@ public class MessageUtil {
   private final CategoryRepository categoryRepository;
 
   public void sendTicketMessage(TextChannel channel, JDA bot) {
-    channel.getIterableHistory().takeAsync(100).thenAccept(messages -> {
-      messages.stream()
-          .filter(m -> m.getAuthor().equals(bot.getSelfUser()))
-          .forEach(m -> m.delete().queue());
-    });
     var embed = new EmbedBuilder()
         .setTitle("Support tickets")
         .setDescription("Report bugs you find on the server or request assistance with any other topic!\nPlease remember to be honest with all reports!\nTo create a ticket, use the button below.")
         .setColor(Color.GREEN)
         .setFooter("DB0S Ticket & Support System - %s categories available".formatted(categoryRepository.findByGuildId(channel.getGuild().getIdLong()).size()))
         .build();
-    try {
-      channel.sendMessageEmbeds(embed)
-          .addActionRow(
-              Button.of(PRIMARY, "ticket_create", "Create ticket", Emoji.fromUnicode("📩"))
-          )
-          .queue();
-    } catch (MissingAccessException ex) {
-      log.error("Cannot send message to channel {}: {}", channel.getId(), ex.getMessage());
-    } catch (Exception ex) {
-      log.error("Unexpected error while sending message to channel {}: {}", channel.getId(), ex.getMessage());
-    }
+
+    channel.getIterableHistory().takeAsync(50).thenAccept(messages -> {
+      var tasks = messages.stream()
+          .filter(m -> m.getAuthor().equals(bot.getSelfUser()))
+          .map(m -> m.delete().submit())
+          .toList();
+
+      CompletableFuture.allOf(tasks.toArray(CompletableFuture[]::new)).thenRun(() -> {
+        try {
+          channel.sendMessageEmbeds(embed)
+              .addActionRow(
+                  Button.of(PRIMARY, "ticket_create", "Create ticket", Emoji.fromUnicode("📩"))
+              )
+              .queue();
+        } catch (MissingAccessException ex) {
+          log.error("Cannot send message to channel {}: {}", channel.getId(), ex.getMessage());
+        } catch (Exception ex) {
+          log.error("Unexpected error while sending message to channel {}: {}", channel.getId(), ex.getMessage());
+        }
+      });
+    });
   }
 
 }
